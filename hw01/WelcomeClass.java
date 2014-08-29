62da9f0163fe7f6e66deb12044f90a4a7bda4416
coc218 <coc218@lehigh.edu>
2014-08-29 14:33:57 +0000

    hw

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+/////////////////////////////////////
+// Cody Chen-Shao 
+// Hw01
+// WelcomeClass Java Program 
+
+public class WelcomeClass{
+    public static void main (String[ ] args) {
+        
+    System.out.println("------------");
+    System.out.println("| Welcome  |");
+    System.out.println("------------");
+    System.out.println("^ ^ ^ ^ ^ ^ ");
+    System.out.println("/ \\ / \\ / \\ / \\ ");
+    System.out.println("|-C-o-c-2-1-8-|");
+    System.out.println("/ \\ / \\ / \\ / \\ ");
+    System.out.println(" v   v   v   ");
+    System.out.println(" I have a pet bunny");
+    System.out.println(" named Jockomo");
+    
+    //test/testtest
+  }
+}    
+    
