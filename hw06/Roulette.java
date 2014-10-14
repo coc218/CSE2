d08f601f4e7fecb227b6b6483f44e8e450cfa1d1
coc218 <coc218@lehigh.edu>
2014-10-14 23:21:28 +0000

    updated hw06

--- hw06/Roulette.java
+++ hw06/Roulette.java
@@ -15,7 +15,7 @@ int guess= (int)(Math.random()*38);
 
 
 
-double money=0; // initialize and declare money
+int money=0; // initialize and declare money
 int winCount = 0; //initialize and declare your win count
 int loseCount = 0; //initialize and declare your lose count
 int profit = 0;  //initialize and declare your profit count
@@ -25,13 +25,14 @@ int counter=0; //initialize and declare your count
 
 while(counter<1000){ //enter 1000 loop
     counter1 = 0; //reset 100 loop back to 0 when entering 1000 loop
-
+    
+    int number=(int)(Math.random()*38); 
+            // generate random number
    int guessCount=0;
-// reset guesscount each 1000 times
+    // reset guesscount each 1000 times
         while(counter1<100){ //100 loop 
 
-            int number=(int)(Math.random()*38); 
-            // generate random number
+           
             if(guess == number){ 
              guessCount++; //increment guessCount
              winCount++;  //increment winCount
@@ -55,7 +56,7 @@ counter++;
 }
 
 System.out.println("The number of times your number never comes up is " +loseCount); 
-System.out.println("Your total winnings is " +money); 
+System.out.println("Your total winnings is $ " +money); 
 System.out.println("The number of times you walked away with profit is " +profit);
 
  
